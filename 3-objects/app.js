9efcf1fce19b8ae80651af58653d0644676cb6fe
Dane <cyber_sky09@yahoo.com>
2022-11-10 17:14:41 +0800

    Code Challeges for Objects

--- 3-objects/app.js
+++ 3-objects/app.js
@@ -2,18 +2,44 @@ const c = console.log.bind(this);
 const dateNow = new Date().getFullYear();
 // Object Construction
 
-const Employee = {
-  firstname: 'John',
-  lastname: 'Smith',
+const avenger = {
+  firstname: 'Tony',
+  lastname: 'Stark',
   age: dateNow - 1966,
-  jobtitle: 'programmer',
+  role: 'Ironman',
+  address: '',
+  status: '',
+  isActive: true,
 };
 
-c(Employee);
+c(avenger);
 
 // Retrieve Data from an Object
-
 // Dot Notation
-c(Employee.firstname);
+c(avenger.firstname);
 // Brackets Notation
-c(Employee['lastname']);
+c(avenger['lastname']);
+
+// Mutate Object data
+avenger.address = 'Manhattan';
+avenger['status'] = 'active';
+c(avenger);
+
+// Object Methods
+const jLeague = {
+  firstname: 'Bruce',
+  lastname: 'Wayne',
+  age: null,
+  birthYear: 1992,
+  role: 'Batman',
+  address: '',
+  status: '',
+  isActive: true,
+  calcAge: function () {
+    this.age = dateNow - this.birthYear;
+    return;
+  },
+};
+
+jLeague.calcAge();
+c(jLeague);
