1b4fb779fbc177d36cb48be98133f0a3af873701
thehungrycoder225 <cyber_sky09@yahoo.com>
2022-11-27 22:15:29 +0800

    Create Objects Exercises Draft

--- part-2-advance/2-3_exercises/app.js
+++ part-2-advance/2-3_exercises/app.js
@@ -1,2 +1,37 @@
 'use strict';
 const c = console.log.bind(this);
+
+// Exercise #1
+// Create an address object with the following properties:
+// street, city, zipCode
+
+// Exercise #2
+// Write a Factory and Constructor function for an address object with the following properties:
+// street, city, zipCode
+
+// Exercise #3
+// Write a function that will check the equality of an address object.
+// the function areEqual(address1, address2) should return true if the address objects are equal.
+// the function areSame(address1, address2) should return true if the address objects are the same object.
+
+// Exercise #4
+// Create a object called blogPost with the following properties:
+// title, body, author, views, comments (author, body), isLive
+
+// Exercise #5
+// Create a constructor function that will create a blogPost object with the following properties:
+// title, body, author, views, comments (author, body), isLive
+// the function should have a method called publish that will set the isLive property to true
+// the function should have a method called unpublish that will set the isLive property to false
+// the function should have a method called addComment that will add a comment to the comments array
+// the function should have a method called removeComment that will remove a comment from the comments array
+
+// Exercise #6
+// Create a function that will determine the priceRange of a product based on the price.
+// the function priceRange(product) should return a string with the price range.
+// the price range should be based on the following:
+// 0 - 10 = cheap
+// 11 - 20 = moderate
+// 21 - 50 = expensive
+// 51 - 100 = very expensive
+// 101 - 1000 = super expensive
