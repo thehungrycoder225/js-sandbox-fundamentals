988716903e1c5f157619570588fb129471ddc882
thehungrycoder225 <cyber_sky09@yahoo.com>
2022-11-28 06:20:19 +0800

    Completed Objects Exercises

--- part-2-advance/2-3_exercises/app.js
+++ part-2-advance/2-3_exercises/app.js
@@ -15,11 +15,11 @@ const c = console.log.bind(this);
 // the function areSame(address1, address2) should return true if the address objects are the same object.
 
 // Exercise #4
-// Create a object called blogPost with the following properties:
+// Create a object called post with the following properties:
 // title, body, author, views, comments (author, body), isLive
 
 // Exercise #5
-// Create a constructor function that will create a blogPost object with the following properties:
+// Create a constructor function that will create a post object with the following properties:
 // title, body, author, views, comments (author, body), isLive
 // the function should have a method called publish that will set the isLive property to true
 // the function should have a method called unpublish that will set the isLive property to false
@@ -27,11 +27,16 @@ const c = console.log.bind(this);
 // the function should have a method called removeComment that will remove a comment from the comments array
 
 // Exercise #6
-// Create a function that will determine the priceRange of a product based on the price.
-// the function priceRange(product) should return a string with the price range.
-// the price range should be based on the following:
-// 0 - 10 = cheap
-// 11 - 20 = moderate
-// 21 - 50 = expensive
-// 51 - 100 = very expensive
-// 101 - 1000 = super expensive
+// Create an array of objects called priceRange
+// each object should have a label and a tooltip
+// label is a string
+// tooltip is a string
+// the array should have 4 objects
+// the first object should have the label '$' and the tooltip 'Inexpensive'
+// the second object should have the label '$$' and the tooltip 'Moderate'
+// the third object should have the label '$$$' and the tooltip 'Pricey'
+// the fourth object should have the label '$$$$' and the tooltip 'Very Expensive'
+// Inexpensive = 0 - 10
+// Moderate = 11 - 20
+// Pricey = 21 - 50
+// Very Expensive = 51+
