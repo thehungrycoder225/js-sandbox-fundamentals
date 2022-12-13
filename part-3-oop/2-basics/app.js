5fcf3c2ff8ab7f2e837ee6a368580888bc394bb3
thehungrycoder225 <cyber_sky09@yahoo.com>
2022-12-13 10:30:44 +0800

    Minor changes on OOP Section

---
+++ part-3-oop/2-basics/app.js
@@ -0,0 +1,17 @@
+// Object Literals
+
+// Factories
+
+// Constructors
+
+// Value vs Reference Types
+
+// Adding or Removing Properties
+
+// Enumerating Properties
+
+// Abstraction
+
+// Private Properties and Methods
+
+// getters and setters
